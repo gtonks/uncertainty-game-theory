07e21d6587203154660b335748f47577a07a301b
gtonks <a02288558@aggies.usu.edu>
2023-04-14 18:53:42 -0600

    Command-line input

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # uncertainty-game-theory
 Uncertainty Assignment 5: Game Theory
+
+
+## Usage
+```
+python main.py [SOURCES DOVES HAWKS]
+```
+When run without arguments, the program defaults to 60 food sources, 119 doves and 1 hawk. Otherwise, enter three integers corresponding the desired number of each item.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import random
 import matplotlib.pyplot as plt
 
@@ -7,9 +8,29 @@ from Part2 import Part2
 
 random.seed(42)
 sim_len = 150
-n_sources = 600
-n_doves = 60
-n_hawks = 60
+n_sources = 60
+n_doves = 119
+n_hawks = 1
+if len(sys.argv) == 4:
+    try:
+        n_sources = int(sys.argv[1])
+        n_doves = int(sys.argv[2])
+        n_hawks = int(sys.argv[3])
+    except ValueError:
+        print("Usage: python main.py [SOURCES DOVES HAWKS]")
+        print("SOURCES, DOVES and HAWKS must be nonnegative integers")
+        exit()
+    try:
+        assert n_sources >= 0
+        assert n_doves >= 0
+        assert n_hawks >= 0
+    except AssertionError:
+        print("Usage: python main.py [SOURCES DOVES HAWKS]")
+        print("SOURCES, DOVES and HAWKS must be nonnegative integers")
+        exit()
+elif len(sys.argv) != 1:
+        print("Usage: python main.py [SOURCES DOVES HAWKS]")
+        exit()
 
 for sim in (Part1(n_sources, n_doves, n_hawks), Part2(n_sources, n_doves, n_hawks)):
     for i in range(sim_len):
